136b79b495a64304244bddd41df77d4c496b0da4
Mark Thebridge <mark.thebridge@gmail.com>
2020-03-28 15:02:06 +0000

    Tweaks ready for part2

--- day7.js
+++ day7.js
@@ -7,24 +7,23 @@ function runAmplifier(program, input, phase) {
 }
 
 // Run the sequence of amplifiers
-function runAmplifierChain(program, phases) {
+function runAmplifierChain(program, phases, feedbackMode) {
     if (phases.length != 5) {
         throw `Invalid phase input ${phases} - expect 5 numbers`
     }
-    
-    let input = 0;
 
+    let input = 0;    
     for (let i = 0; i < phases.length; i++) {
-        input = runAmplifier(program, input, phases[i])    
+        input = runAmplifier(program, input, phases[i])
     }
     return input
 }
 
-function findMaxPhaseSetting(program) {
-    
-    function *findPerms(list) {
+function findMaxPhaseSetting(program, feedbackMode) {
+
+    function* findPerms(list) {
         for (let i = 0; i < list.length; i++) {
-            const value = list[i];  
+            const value = list[i];
             // console.debug("Have value", value)
             let rest = list.slice()
             rest.splice(i, 1)
@@ -39,21 +38,26 @@ function findMaxPhaseSetting(program) {
             }
         }
     }
-    
+
     // Check all possible phase settings.
-    const phaseValues = [0, 1, 2, 3, 4];
+    let phaseValues = [];
+    if (feedbackMode) {
+        phaseValues = [5, 6, 7, 8, 9];
+    } else {
+        phaseValues = [0, 1, 2, 3, 4];
+    }
     let maxSoFar = 0;
     let maxPerm = phaseValues;
     for (const permutation of findPerms(phaseValues)) {
         console.debug("Trying permutation", permutation)
-        let out = parseInt(unAmplifierChain(program, permutation))
-        if (out  > maxSoFar) {
+        let out = parseInt(runAmplifierChain(program, permutation, feedbackMode))
+        if (out > maxSoFar) {
             console.log("new best output", out)
             maxSoFar = out
             maxPerm = permutation
         }
     }
-    
+
     return [maxPerm, maxSoFar]
 
 }
@@ -70,9 +74,9 @@ function run_day7() {
 
 function tests_day7() {
     let passes = 0;
-    passes += test_assert("1.1", findMaxPhaseSetting([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])[1], 43210)
-    passes += test_assert("1.2", findMaxPhaseSetting([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0])[1], 54321)
-    passes += test_assert("1.3", findMaxPhaseSetting([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])[1], 65210)
+    passes += test_assert("1.1", findMaxPhaseSetting([3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0], false)[1], 43210)
+    passes += test_assert("1.2", findMaxPhaseSetting([3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0], false)[1], 54321)
+    passes += test_assert("1.3", findMaxPhaseSetting([3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0], false)[1], 65210)
     document.getElementById("tests7").innerHTML = `${passes}/3 tests passed!`;
 
 }
