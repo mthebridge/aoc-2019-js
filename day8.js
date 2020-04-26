dc08a28d436d5f073df68fbab413e91148a1d06d
Mark Thebridge <mark.thebridge@gmail.com>
2020-04-26 23:36:18 +0100

    Day 8 complete

---
+++ day8.js
@@ -0,0 +1,87 @@
+import { read_text_input, test_assert } from "./utils.js";
+
+const IMAGE_HEIGHT = 6;
+const IMAGE_WIDTH = 25;
+const LAYER_SIZE = IMAGE_HEIGHT * IMAGE_WIDTH; 
+
+
+function countDigitInArray(input, digit) {    
+    return input.reduce((total, value) => { if (value == digit) { return total + 1 } else { return total } }, 0)
+}
+
+// split the image into layers
+function get_layer_with_fewest_zeros(input) {
+    let idx = 0;
+    let bestSoFar = {size: LAYER_SIZE, layer: null };
+    for (idx = 0; idx < input.length; idx += LAYER_SIZE) {
+        let nextLayer = input.slice(idx, idx + LAYER_SIZE);
+        let zeroCount = countDigitInArray(nextLayer, "0")
+        console.debug("layer index, num zeroes", idx, zeroCount)
+        if (zeroCount < bestSoFar.size) {
+            bestSoFar = {size: zeroCount, layer: nextLayer }
+            console.debug("New best: layer idx, zeroes", idx, zeroCount)
+        }
+        
+    }
+    return bestSoFar
+}
+
+function getFinalPixelColours(input) {
+    // initialize the image as transpaent.
+    let image = Array.from('2'.repeat(LAYER_SIZE))
+    // Iterate over the layers
+    for (let idx = 0; idx < input.length; idx += LAYER_SIZE) {
+        let nextLayer = input.slice(idx, idx + LAYER_SIZE);
+        // If we haven't yet found the colour for this pixel, set it.
+        // The first non-transaprent (ie not 2) value is what we want
+        nextLayer.forEach((colour, pxl) => {                  
+            if (image[pxl] == "2") {
+                image[pxl] = colour
+            }
+        })
+    }
+
+    // Check the image has not transparent fields!
+    image.forEach((colour) => {if (colour == "2") {throw "Transaprertn pixel in image!"} })
+    return image
+}
+
+function renderImage(imageData)  {
+    // Create a simple div!
+    let outHtml = "<pre>\n"
+    imageData.forEach((colour, index) => {                  
+        let colourString;
+        if (colour == "0") {
+            colourString = "white"
+        }
+        if (colour == "1") {
+            colourString = "black"
+        }
+        outHtml += `<span style="background-color:${colourString}"> </span>`
+        if ((index % IMAGE_WIDTH) == IMAGE_WIDTH - 1) {
+            outHtml += "\n"
+        }
+    })
+    outHtml += "\n</pre>"
+    return outHtml
+    
+}
+
+function run_day8() {
+    read_text_input("inputs/day8.txt", (input) => {
+        let inputArray = input.split("")
+        let layer = get_layer_with_fewest_zeros(inputArray).layer
+        console.log("Layer is:", layer)
+        let answer = countDigitInArray(layer, "1") * countDigitInArray(layer, "2")
+        let image = renderImage(getFinalPixelColours(inputArray))
+        document.getElementById("day8").innerHTML = `Answer for part1: ${answer}, image: ${image}`;
+    })
+}
+
+function tests_day8() {
+    let passes = 0;
+    document.getElementById("tests8").innerHTML = `${passes}/0 tests passed!`;
+
+}
+
+export { run_day8, tests_day8 };
--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 
 /// Initialiation
 const NUM_DAYS = 25;
-const AVAILABLE_DAYS = 7    ;
+const AVAILABLE_DAYS = 8;
 
 import { run_day1, tests_day1 } from './day1.js';
 import { run_day2, tests_day2 } from './day2.js';
@@ -10,6 +10,7 @@ import { run_day4, tests_day4 } from './day4.js';
 import { run_day5, tests_day5 } from './day5.js';
 import { run_day6, tests_day6 } from './day6.js';
 import { run_day7, tests_day7 } from './day7.js';
+import { run_day8, tests_day8 } from './day8.js';
 
 export function setup() {
     console.log("Running setup")
@@ -61,6 +62,10 @@ export function setup() {
             case 7:
                 runFn = run_day7;
                 testFn = tests_day7;
+                break;
+            case 8:
+                runFn = run_day8;
+                testFn = tests_day8;
                 break;
             
             default:
